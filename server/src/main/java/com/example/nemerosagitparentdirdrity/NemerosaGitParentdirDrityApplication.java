70bfed5b9beec79c0d0e2f2c4b6a7309a8bf8bda
Remo Loetscher <remo.loetscher@bison-group.com>
2022-06-08 14:42:20 +0200

    add spring boot initializer

---
+++ server/src/main/java/com/example/nemerosagitparentdirdrity/NemerosaGitParentdirDrityApplication.java
@@ -0,0 +1,13 @@
+package com.example.nemerosagitparentdirdrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemerosaGitParentdirDrityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemerosaGitParentdirDrityApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/nemerosagitparentdirdrity/NemerosaGitParentdirDrityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nemerosagitparentdirdrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemerosaGitParentdirDrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
